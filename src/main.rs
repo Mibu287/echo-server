e8bcb0dd43c58435041dbcdc517521e50e6fdfcf
Mibu287 <buiducminh287@gmail.com>
2022-12-06 12:31:52 +0700

    Implement echo-server

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{App, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[actix_web::get("/{message}")]
+async fn echo(message: actix_web::web::Path<String>) -> impl Responder {
+    format!("Echo: {}", message.as_str())
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(echo))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
